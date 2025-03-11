8d069e8f870edc117b5ac18777a5e65d9c00d41b
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 <vergilwang1225@gmail.com>
2025-03-11 15:18:35 -0600

    adding Coming Soon to checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -34,7 +34,7 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleCheckoutShopping = (e) => {
-        alert('Functionality to be added for future reference');
+        alert('Coming Soon! Functionality to be added for future reference');
     };
 
     const handleIncrement = (item) => {
